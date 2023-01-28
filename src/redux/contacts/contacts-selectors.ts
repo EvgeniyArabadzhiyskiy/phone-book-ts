39c98d4840eb634448d7af4a97538d41824cdd27
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-01-28 19:09:10 +0200

    Create contacts-selectors.ts

---
+++ src/redux/contacts/contacts-selectors.ts
@@ -0,0 +1,19 @@
+export const a = 5
+// const getItems = state => state.contacts.items;
+// const getFilter = state => state.contacts.filter;
+
+// const getVisibleContacts = state => {
+//   const items = getItems(state);
+//   const filterContacts = getFilter(state);
+
+//   const normalizedFilter = filterContacts.toLowerCase();
+//   return items.filter(contact =>
+//     contact.name.toLowerCase().includes(normalizedFilter)
+//   );
+// };
+
+// export const contactsSelectors = {
+//   getItems,
+//   getFilter,
+//   getVisibleContacts,
+// };
